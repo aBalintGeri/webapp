92f290ebc8c06a81768bee4a5115e08550735dcd
zbalint <balint.zoltan.marton@gmail.com>
2020-05-22 13:31:14 +0200

    fájlok formázása

--- scripts.js
+++ scripts.js
@@ -1,10 +1,10 @@
   jQuery.fn.exists = function() {
-    return this.length > 0;
+      return this.length > 0;
   }
-  
+
   $(".loaderPage").show();
 
   $(window).one("load", function() {
-    // betöltő képernyő eltüntetése
-    $(".loaderPage").hide();
+      // betöltő képernyő eltüntetése
+      $(".loaderPage").hide();
   });
